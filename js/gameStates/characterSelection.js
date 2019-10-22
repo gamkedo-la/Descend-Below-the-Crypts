bee78e7ad70d3eb114faabfb5e8e612d2d0fefd8
vmckeown <vmckeown79@gmail.com>
2019-10-22 12:13:32 -0400

    Updates to the Character Selection Page

--- js/gameStates/characterSelection.js
+++ js/gameStates/characterSelection.js
@@ -10,9 +10,20 @@ var characterBoxWidth = 155;
 function drawCharacterSelectionPage(){
 	colorRect(0,0,canvas.width,canvas.height, 'white');
 	canvasContext.drawImage(characterSelectionBackgroundPic,0, 0);
+	colorText("Choose your Hero", 150, 200, 'red', "48px Arial Black");
 	colorText("The Warrior", warriorBoxX+45, 287, 'white', "12px Arial Black");
 	colorText("The Wizard", wizardBoxX+45, 287, 'white', "12px Arial Black");
 	colorText("The Cleric", clericBoxX+50, 287, 'white', "12px Arial Black");
+	/////////////////////////   Art to be replaced /////////////////////////////////
+	colorText("To be replaced with", warriorBoxX+15, warriorBoxY+90, 'white', "12px Arial Black");
+	colorText("Warrior Profile Pic", warriorBoxX+20, warriorBoxY+110, 'white', "12px Arial Black");
+	colorText("To be replaced with", wizardBoxX+15, wizardBoxY+90, 'white', "12px Arial Black");
+	colorText("Wizard Profile Pic", wizardBoxX+20, wizardBoxY+110, 'white', "12px Arial Black");
+	colorText("To be replaced with", clericBoxX+15, clericBoxY+90, 'white', "12px Arial Black");
+	colorText("Cleric Profile Pic", clericBoxX+20, clericBoxY+110, 'white', "12px Arial Black");
+	
+	
+	
 }
 
 function characterSelectionPageMouseClick(mousePosX, mousePosY) {
