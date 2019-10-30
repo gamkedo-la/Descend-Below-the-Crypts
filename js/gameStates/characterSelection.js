6f3095d55acd2a14ee8f9b9ff3b3bb7a8a4b0984
vmckeown <vmckeown79@gmail.com>
2019-10-29 21:51:05 -0400

    Refactored Character Selection

--- js/gameStates/characterSelection.js
+++ js/gameStates/characterSelection.js
@@ -4,44 +4,71 @@ var wizardBoxX = 338;
 var wizardBoxY = 313;
 var clericBoxX = 570;
 var clericBoxY = 313;
-var characterBoxHeight = 205;
 var characterBoxWidth = 155;
+var characterBoxHeight = 205;
+var buttonList = [
+	{
+	cornerX: warriorBoxX, 
+	cornerY: warriorBoxY,
+	width: characterBoxWidth,
+	height: characterBoxHeight,
+	image: warriorProfilePic,
+	debugName: "Warrior",
+	func: function(){
+		startWithCharacter(warriorPic, "The Warrior");
+		}
+	},
+	{
+	cornerX: wizardBoxX, 
+	cornerY: wizardBoxY,
+	width: characterBoxWidth,
+	height: characterBoxHeight,
+	image: wizardProfilePic,
+	debugName: "Wizard",
+	func: function(){
+		startWithCharacter(wizardPic, "The Wizard");
+		}
+	},
+	{
+	cornerX: clericBoxX, 
+	cornerY: clericBoxY,
+	width: characterBoxWidth,
+	height: characterBoxHeight,
+	image: clericProfilePic,
+	debugName: "Cleric",
+	func: function(){
+		startWithCharacter(clericPic, "The Cleric");
+		}
+	}
+];
+function startWithCharacter(charImage, charName){
+	playerOne.init(charImage, charName);			
+	updateGameState();
+	liveGame = true;
+	basementMusic.loopSong("Decrepit_Basement_draft1");
+}
+
+
+
 
 function drawCharacterSelectionPage(){
 	canvasContext.drawImage(characterSelectionBackgroundPic,0, 0);
 	colorText("Choose your Hero", 175, 225, 'black', "48px Arial Black");
-	colorText("The Warrior", warriorBoxX+45, 287, 'white', "12px Arial Black");
-	colorText("The Wizard", wizardBoxX+45, 287, 'white', "12px Arial Black");
-	colorText("The Cleric", clericBoxX+50, 287, 'white', "12px Arial Black");
-	/////////////////////////   Art to be replaced /////////////////////////////////
-	canvasContext.drawImage(warriorProfilePic,warriorBoxX, warriorBoxY);
-	canvasContext.drawImage(wizardProfilePic,wizardBoxX, wizardBoxY);
-	canvasContext.drawImage(clericProfilePic,clericBoxX, clericBoxY);	
+	for(var i = 0; i < buttonList.length; i++){
+		canvasContext.drawImage(buttonList[i].image,buttonList[i].cornerX,buttonList[i].cornerY);
+		colorText(buttonList[i].debugName, buttonList[i].cornerX+45, buttonList[i].cornerY - 25, 'white', "12px Arial Black");
+	}
 }
 
 function characterSelectionPageMouseClick(mousePosX, mousePosY) {
-	
-	if(		mousePosX > warriorBoxX && mousePosX < warriorBoxX + characterBoxWidth && 
-			mousePosY > warriorBoxY && mousePosY < warriorBoxY + characterBoxHeight){
-					playerOne.init(warriorPic, "The Warrior");			
-					updateGameState();
-					liveGame = true;
-					basementMusic.loopSong("Decrepit_Basement_draft1");
-	} else if(	mousePosX > wizardBoxX && mousePosX < wizardBoxX + characterBoxWidth && 
-				mousePosY > wizardBoxY && mousePosY < wizardBoxY + characterBoxHeight){
-					playerOne.init(wizardPic, "The Wizard");
-					updateGameState();
-					liveGame = true;
-					basementMusic.loopSong("Decrepit_Basement_draft1");					
-	} else if(	mousePosX > clericBoxX && mousePosX < clericBoxX + characterBoxWidth && 
-				mousePosY > clericBoxY && mousePosY < clericBoxY + characterBoxHeight){
-					playerOne.init(clericPic, "The Cleric");
-					updateGameState();
-					liveGame = true;
-					basementMusic.loopSong("Decrepit_Basement_draft1");
-	} else {
-		colorRect(0,0,canvas.width,canvas.height, 'red');
+	for(var i = 0; i < buttonList.length; i++){
+		if(		mousePosX > buttonList[i].cornerX && mousePosX < buttonList[i].cornerX + buttonList[i].width && 
+			mousePosY > buttonList[i].cornerY && mousePosY < buttonList[i].cornerY + buttonList[i].height){
+			buttonList[i].func();
+			return;
+		}
 	}
-	
+	//Flash Red if invalid click
+	colorRect(0,0,canvas.width,canvas.height, 'red');	
 }
 			
