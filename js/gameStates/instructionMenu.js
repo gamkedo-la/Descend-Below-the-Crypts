7e4704aee5bae0d0a23f20c3e845f9708a306d9a
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2019-12-07 01:28:02 -0600

    Refactor and add more content to instruction menu

--- js/gameStates/instructionMenu.js
+++ js/gameStates/instructionMenu.js
@@ -2,24 +2,63 @@ class InstructionMenu extends GameState {
   constructor() {
     super();
   }
-
+  /**
+   *  Draws all content
+   */
   draw() {
     super.draw();
-
-    canvasContext.drawImage(mainMenuPic, 0, 0);
-    canvasContext.drawImage(instructionScreenPic, 10, 100);
-    canvasContext.drawImage(titleBarPic, 10, 25);
-
-    colorText("Instructions", 50, 80, 'black', "48px Arial Black");
+    this.drawImages();
+    this.drawText();
   }
 
-  onMouseClick(mouseX, mouseY) {
-    if (mouseX > 0 && mouseX < 800 &&
-  			mouseY > 0 && mouseY < 600) {
-  	   gameStateManager.setState(State.MENU);
-  	} else {
-  	   colorRect(0,0,canvas.width,canvas.height, 'red');
+  /**
+   *  Mouse click event handler
+   * 
+   * @param {number} mouseXPosition 
+   * @param {number} mouseYPosition
+   */
+  onMouseClick( mouseXPosition, mouseYPosition ) {
+    if ( this.isMouseOnCanvas( mouseXPosition, mouseYPosition ) ) {
+  	   this.returnToMainMenu();
   	}
+  }
+
+  /**
+   *  Draw all Images
+   */
+  drawImages() {
+    canvasContext.drawImage( mainMenuPic, 0, 0 );
+    canvasContext.drawImage( instructionScreenPic, 10, 100 );
+    canvasContext.drawImage( titleBarPic, 10, 25 );
+  }
+
+  /**
+   *  Draw all text
+   */
+  drawText() {
+      colorText( "Instructions", 50, 80, 'black', '48px Arial Black' );
+      colorText( "To move: WASD", 50, 130, 'white', '28px Arial Black' );
+      colorText( "To Attack: Click Weapon in Lower HUD", 50, 170, 'white', '28px Arial Black' );
+  }
+
+  /**
+   * Check if mouse is on canvas 
+   *
+   * @param {number} mouseXPosition
+   * @param {number} mouseYPosition
+   * 
+   * @returns {boolean}
+   */
+  isMouseOnCanvas( mouseXPosition, mouseYPosition ) {
+    return mouseXPosition > 0 && mouseXPosition < canvas.width &&
+           mouseYPosition > 0 && mouseYPosition < canvas.height;
+  }
+
+  /**
+   *  Sets State to main menu
+   */
+  returnToMainMenu() {
+    gameStateManager.setState(State.MENU);
   }
 
   onMouseMove(mouseX, mouseY) {
